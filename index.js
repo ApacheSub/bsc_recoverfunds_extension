c307ec369507bb92ecbd4f06647914d96292a82a
Jami <qiqhsqwmo@mozmail.com>
2023-03-11 21:05:25 +0200

    Refactoring: Moved main functionality of program to a class. The program also now covers more derivation paths and is more easily customizable.

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const {findAddress} = require("./recoverFunds_manypaths");
+// Donations are welcome
+
+// BNB
+// bnb1eyvjl0zmp0wke36gxts4hu3wtgtnpw5dnj5ja3
+
+// Ethereum / Ethereum Tokens / BNB / Binance Smart Chain Tokens
+// 0x5Cd8C416d3061398b868dC2a5C835C9865caDc7E
+
+// Litecoin
+// ltc1qel8yaqz34pqqp87eey7zx0zz6jkk82wu65en6v
+
+// Bitcoin
+// bc1q75yh6ttwxazfhxnw3cwpqztvrkttjsh2a73mcy
+
+const { AddressFinder } = require("./recoverFunds_manypaths");
 
 // Fill in values for following variables:
 // Enter the address you want to find (the one where your funds are):
-const targetAddr = "REPLACETHISWITHTARGETADDRESS".toLowerCase();
+const TARGET_ADDR = "REPLACETHISWITHTARGETADDRESS".toLowerCase();
 
-// Fill in the Ledger Recovery Phrase (or other mnemonic, if relevant) below (24 words)
-const mnemonic = "REPLACETHISWITHMNEMONIC";
+// Fill in the Ledger Recovery Phrase (or other mnemonic, if relevant) below (24 words for example)
+const MNEMONIC = "REPLACETHISWITHMNEMONIC";
 
 // And optionally
 // If you used a passphrase on your Ledger Device (the "25th" word),
 // Enter it below or otherwise leave as is
 // Leave unchanged if passphrase is not being used
-const passphrase = "";
+const PASSPHRASE = "";
 
-findAddress(mnemonic, targetAddr, passphrase);
+const MAX_DEPTH = 2;
+const MAX_PATH_INDEX = 100;
+const MAX_CHILD_INDEX = 100;
+
+let finder = new AddressFinder(MNEMONIC, PASSPHRASE, MAX_DEPTH, MAX_PATH_INDEX, MAX_CHILD_INDEX);
+finder.findAddress(TARGET_ADDR);
--- recoverFunds_manypaths.js
+++ recoverFunds_manypaths.js
@@ -29,53 +29,103 @@ const { getPublicKey } = require("ethereum-cryptography/secp256k1");
 
 const { toHex } = require("ethereum-cryptography/utils");
 
-function getAddress(privateKey) {
-  const uncomprPub = getPublicKey(privateKey, false);
-  return `0x${toHex(keccak256(uncomprPub.slice(1)).slice(-20))}`;
-}
+class AddressFinder {
+  #seed;
+  #hdkey;
 
-function tryPath(parent, path, targetAddr, childIterations = 50) {
-  parentAddr = getAddress(parent.privateKey);
-  for(let j = 0; j < childIterations; j++) {
-    child = parent.deriveChild(j);
-    childAddr = getAddress(child.privateKey);
-    if(childAddr == targetAddr) {
-      console.log(`Parent address: ${parentAddr}`);
-      console.log(`Derivation path ${path}/${child.index}`);
-      console.log(`Child ${j} address: ${childAddr}`);
-      console.log(`Child Private Key: 0x${toHex(child.privateKey)}`);
-      console.log("---");
+  #maxDepth;
+  #maxPathIndex;
+  #maxChildIndex;
+  
+  // Not needed currently
+  //#alphabet = [...Array(26).keys()].map((n) => String.fromCharCode(97 + n));
+  #regularPaths = [
+    // Derivation path not supported by the ethereum-cryptography library
+    ["m/44'/60'/0'/", ""],
+    ["m/44'/60'/", "'/0/0"],
+    ["m/44'/60'/0'/0/", ""]
+  ];
+
+  constructor(mnemonic, passphrase, maxDepth=2, maxPathIndex=100, maxChildIndex=100) {
+    this.#seed = mnemonicToSeedSync(mnemonic, passphrase);
+    this.#hdkey = HDKey.fromMasterSeed(this.#seed);
+    this.#maxDepth = maxDepth;
+    this.#maxPathIndex = maxPathIndex;
+    this.#maxChildIndex = maxChildIndex;
+  }
+
+  #printResult(addr, path, privKey) {
+    console.log("---");
+    console.log(`Address: ${addr}`);
+    console.log(`Derivation path ${path}`);
+    console.log(`Private Key: 0x${toHex(privKey)}`);
+    console.log("---");
+  }
+
+  #getAddress(privateKey) {
+    const uncomprPub = getPublicKey(privateKey, false);
+    return `0x${toHex(keccak256(uncomprPub.slice(1)).slice(-20))}`;
+  }
+
+  #tryPath(path, targetAddr) {
+    let parent = this.#hdkey.derive(path);
+    let parentAddr = this.#getAddress(parent.privateKey);
+    if(parentAddr === targetAddr) { 
+      this.#printResult(parentAddr, path, parent.privateKey);
       return 1;
     }
-  }
-  return 0;
-}
-
-function findAddress(mnemonic, targetAddr, passphrase = "") {
-  const pathIterations = 50;
-
-  const seed = mnemonicToSeedSync(mnemonic, passphrase);
-  const hdkey = HDKey.fromMasterSeed(seed);
-
-  let path;
-  let res;
-
-  for(let k = 0; k < pathIterations; k++) {
-    path = `m/44'/60'/${k}'`
-    res = tryPath(hdkey.derive(path), path, targetAddr);
-    if(res === 1) { return; }
-    for(let i = 0; i < pathIterations; i++) {
-      path = `m/44'/60'/${k}'/${i}`;
-      res = tryPath(hdkey.derive(path), path, targetAddr);
-      if(res === 1) { return; }
-      for(let j = 0; j < pathIterations; j++) {
-        path = `m/44'/60'/${k}'/${i}/${j}`;
-        res = tryPath(path, targetAddr);
-        if(res === 1) { return; }
+    let child, childAddr;
+    for(let j = 0; j <= this.#maxChildIndex; j++) {
+      child = parent.deriveChild(j);
+      childAddr = this.#getAddress(child.privateKey);
+      if(childAddr === targetAddr) {
+        this.#printResult(childAddr, `${path}/${child.index}`, child.privateKey);
+        return 1;
       }
     }
+    return 0;
   }
-  console.log("Address was not found.");
+
+  #tryRegularPaths(targetAddr) {
+    for(const element of this.#regularPaths) {
+      for(let i = 0; i <= this.#maxPathIndex; i++) {
+        if(this.#tryPath(element[0] + i + element[1], targetAddr) === 1) {
+          return;     
+        };
+      }
+    };
+  }
+
+  #recurseIrregularPaths(targetAddr, paths = [["m/44'/60'/", 0, 0]]) {
+    if(paths.length > 0) {
+      const [path, index, depth] = paths.pop();
+      if(index <= this.#maxPathIndex) {
+        if(this.#tryPath(`${path}${index}`, targetAddr) === 1 ||
+        this.#tryPath(`${path}${index}'`, targetAddr) === 1) {
+          return;
+        }
+        // Add path back to the array as long as maxPathIndex 
+        // will not be exceeded
+        if(index < this.#maxPathIndex) { paths.push([path, index+1, depth]); }
+        // Increase depth of current path if maxDepth not exceeded
+        if(depth <= this.#maxDepth) {
+          paths.push([`${path}${index}/`, 0, depth+1]);
+          paths.push([`${path}${index}'/`, 0, depth+1]);
+        }
+        return this.#recurseIrregularPaths(targetAddr, paths);
+      }
+    } else {
+      return;
+    }
+  }
+
+  findAddress(targetAddr) {
+    //this.#tryRegularPaths(targetAddr);
+    this.#recurseIrregularPaths(targetAddr);
+    // Example of adding another coin type to be recursed
+    //this.#recurseIrregularPaths(targetAddr, [["m/44'/60'/", 0, 0], ["m/44'/61'/", 0, 0]]);
+  }
+
 }
 
-module.exports = { findAddress }
+module.exports = { AddressFinder }
