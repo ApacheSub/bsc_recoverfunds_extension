c871e504fac1a6d6bac949ed77f0d61e8971b481
Jami <qiqhsqwmo@mozmail.com>
2023-03-11 22:19:11 +0200

    Added TARGET_ADDR back to example_index.js after removing it for testing

--- example_index.js
+++ example_index.js
@@ -20,7 +20,7 @@ const { AddressFinder } = require("./recoverFunds_manypaths");
 
 // Fill in values for following variables:
 // Enter the address you want to find (the one where your funds are):
-const TARGET_ADDR = "".toLowerCase();
+const TARGET_ADDR = "0xc79bfC83D301031FE4aC559F66a0e1a7D20f4c38".toLowerCase();
 // Fill in the Ledger Recovery Phrase (or other mnemonic, if relevant) below (24 words)
 const MNEMONIC = "congress fury organ twenty text cruise sample ugly butter dog until air already flock stool master foil jaguar hint trophy brief remind cake bridge";
 // And optionally
